581bff1883026ae55c83b257b2105d038bc694f1
dgiraldom95 <d.giraldo12@uniandes.edu.co>
2019-03-17 21:09:01 -0500

    Endpoint calificacion producto

--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+
 from api.serializers import *
 from api.models import *
 from rest_framework.decorators import action
@@ -9,9 +11,22 @@ class CDTViewSet(viewsets.ModelViewSet):
     serializer_class = CDTSerializer
     permission_classes = (permissions.AllowAny,)
 
-    @action(detail=True, methods=['post'])
-    def post_calificaciones(self, request):
-        pass
+    @action(detail=True)
+    def calificaciones(self, request, pk=None):
+        cdt = CDT.objects.get(pk=pk)
+        calificacionesModel = CalificacionProducto.objects.filter(producto=cdt.productoBancario_id)
+        calificacionSerializer = CalificacionProductoSerializer(calificacionesModel, many=True)
+        return Response(calificacionSerializer.data)
+
+    @calificaciones.mapping.post
+    def postCalificaciones(self, request, pk=None):
+        request.data['producto'] = pk
+        serializer = CalificacionProductoSerializer(data=request.data)
+        if serializer.is_valid():
+            calificacion = serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CalificacionBancoViewSet(viewsets.ModelViewSet):
