af9317aa6891ce5e64c6352d88cdbb5cab879431
dgiraldom95 <d.giraldo12@uniandes.edu.co>
2019-03-30 22:17:04 -0500

    GET CDTs sin params

--- api/views.py
+++ api/views.py
@@ -18,43 +18,53 @@ class CDTViewSet(viewsets.ModelViewSet):
         queryset = self.filter_queryset(self.get_queryset())
         params = self.request.query_params
 
-        monto = params['monto']
-        plazo = params['plazo']
-        fecha = datetime.datetime.today().strftime('%Y-%m-%d')
+        if 'monto' in params and 'plazo' in params:
+            monto = params['monto']
+            plazo = params['plazo']
+            fecha = datetime.datetime.today().strftime('%Y-%m-%d')
 
-        sql = """ SELECT api_cdt.id, api_cdt.plazo_min_dias, api_cdt.tasa, api_cdt.monto, api_cdt.monto_minimo, api_cdt.producto_bancario_id
-                    FROM api_cdt,  
-                            api_productobancario productobancario,
-                            (SELECT prod.banco_id banco, max(cdt.tasa) tasa
-                                FROM api_cdt cdt,
-                                    api_productobancario prod
-                                WHERE (
-                                 (cdt.monto_minimo <= %s OR cdt.monto_minimo IS NULL)
-                                   AND (cdt.monto <= %s OR monto IS NULL)
-                                   AND (cdt.producto_bancario_id = prod.id)
-                                   AND (cdt.plazo_min_dias <= %s)
-                                   AND (prod.fecha = %s)
-                                    )
-                                GROUP BY prod.banco_id
-                            ) maxTasa
-                    WHERE api_cdt.tasa = maxTasa.tasa
-                       AND api_cdt.producto_bancario_id = productobancario.id
-                       AND productobancario.banco_id = maxTasa.banco
-                    ORDER BY api_cdt.tasa DESC;
-             """
+            sql = """ SELECT api_cdt.id, api_cdt.plazo_min_dias, api_cdt.tasa, api_cdt.monto, api_cdt.monto_minimo, api_cdt.producto_bancario_id
+                        FROM api_cdt,  
+                                api_productobancario productobancario,
+                                (SELECT prod.banco_id banco, max(cdt.tasa) tasa
+                                    FROM api_cdt cdt,
+                                        api_productobancario prod
+                                    WHERE (
+                                     (cdt.monto_minimo <= %s OR cdt.monto_minimo IS NULL)
+                                       AND (cdt.monto <= %s OR monto IS NULL)
+                                       AND (cdt.producto_bancario_id = prod.id)
+                                       AND (cdt.plazo_min_dias <= %s)
+                                       AND (prod.fecha = %s)
+                                        )
+                                    GROUP BY prod.banco_id
+                                ) maxTasa
+                        WHERE api_cdt.tasa = maxTasa.tasa
+                           AND api_cdt.producto_bancario_id = productobancario.id
+                           AND productobancario.banco_id = maxTasa.banco
+                        ORDER BY api_cdt.tasa DESC;
+                 """
 
-        query = CDT.objects.raw(sql, params=[monto, monto, plazo, fecha])
-        prefetch_related_objects(query, 'producto_bancario')
-        prefetch_related_objects(query, 'producto_bancario__banco')
+            query = CDT.objects.raw(sql, params=[monto, monto, plazo, fecha])
+            prefetch_related_objects(query, 'producto_bancario')
+            prefetch_related_objects(query, 'producto_bancario__banco')
 
-        dicNoRepetir = {}
-        listaFinal = []
-        for cdt in query:
-            if cdt.banco not in dicNoRepetir:
-                dicNoRepetir[cdt.banco] = True
-                listaFinal.append(cdt)
+            dicNoRepetir = {}
+            listaFinal = []
+            for cdt in query:
+                if cdt.banco not in dicNoRepetir:
+                    dicNoRepetir[cdt.banco] = True
+                    listaFinal.append(cdt)
 
-        serializer = self.get_serializer(listaFinal, many=True)
+            serializer = self.get_serializer(listaFinal, many=True)
+
+        else:
+            queryset = self.filter_queryset(self.get_queryset())
+            page = self.paginate_queryset(queryset)
+            if page is not None:
+                serializer = self.get_serializer(page, many=True)
+                return self.get_paginated_response(serializer.data)
+
+            serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
     @action(detail=True)
